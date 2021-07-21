4461ca09b3491b8dac2f652441b8cd0427501253
baongoc9x01 <85268919+baongoc9x01@users.noreply.github.com>
2021-07-21 21:13:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: 'Bây bê !😘',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    introDesc: `Phố đã lên đèn
+    con đường có môi kề môi
+    Phố đã lên đèn
+    bên người anh thương, anh nhớ, anh mong`,
+    btnIntro: 'Ahihi, đồ ngốc',
+    title: 'Có nhạc bên cạnh ai không phiêu, Có em bên cạnh yêu không phai 😙',
+    desc: 'Tình yêu nằm ngang của anh luôn ngàn năm ',
+    btnYes: 'Vẫn cứ là thích anh, Yêu em <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Tối nay rảnh hông, anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/haikyuu9x/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
